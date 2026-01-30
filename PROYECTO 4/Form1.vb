f23e43b965b47f3d92c9b55b1e08e5d25be4a3c0
ian-robles67 <ian.robles@iest.edu.mx>
2026-01-30 08:24:33 -0600

    Se añadió la opcion de resta (originalmente suma)
    leímos dos números, los convertimos de string a integer, hicimos la suma, convertimos el resultado en string y lo mostramos en pantalla

---
+++ PROYECTO 4/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Label1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Label2 As Integer = Integer.Parse(TextBox2.Text)
+
+        Dim Label3 As Integer = Label1 - Label2
+
+        Label4.Text = Label3.ToString()
+    End Sub
+End Class
